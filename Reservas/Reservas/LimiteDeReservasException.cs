c2f2a16aa67469cdacf69ccb9b1b49b01920a317
Guilherme Oliveira <guilhermego1997@gmail.com>
2024-03-15 12:47:16 -0300

    Aula de exceptions

---
+++ Reservas/Reservas/LimiteDeReservasException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservas
+{
+    public class LimiteDeReservasException : Exception
+    {
+        public LimiteDeReservasException(string message) : base(message) { }
+
+        public LimiteDeReservasException(string message, Exception exception) : base(message, exception) { }    
+    }
+}
---
+++ Reservas/Reservas/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Reservas;
+
+try
+{
+    string[] nomes = new string[2];
+
+    nomes[0] = "guilherme";
+    nomes[1] = "lucas";
+
+    if (nomes.Length == 2)
+    {
+        throw new LimiteDeReservasException("Limite maximo de reservas");
+    }
+    nomes[2] = "Mathues";
+}
+
+catch (NullReferenceException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    string nome = null;
+
+    Console.WriteLine(nome.Length);
+}
+catch (NullReferenceException ex)
+{
+    Console.WriteLine("Erro ao executar programa");
+}
+
+try
+{
+    Console.WriteLine("Digite um valor");
+    string input = Console.ReadLine();
+    int valor = 10 / int.Parse(input);
+    Console.WriteLine(valor);
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Erro");
+}
+finally
+{
+    Console.WriteLine("Fluxo que segue");
+}
+
+
+
+
+
